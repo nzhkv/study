64dca71c9304e2f36bd4bd09fbe72a8ec757993b
Nikolay Zhukov <nzhukov.rnd@gmail.com>
2022-05-12 19:18:54 +0300

    3

--- WeeklyFinder/WeeklyFinder/ViewController.swift
+++ WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -14,5 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var monthTF: UITextField!
     @IBOutlet weak var yearTF: UITextField!
     
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: Any) {
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTF.text!)
+        dateComponents.month = Int(monthTF.text!)
+        dateComponents.year = Int(yearTF.text!)
+        
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "EEEE"
+        
+        let date = calendar.date(from: dateComponents)
+        
+        let weekday = dateFormater.string(from: date!)
+        
+        resultLabel.text = weekday
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
