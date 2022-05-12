e2bddc171b713abab4fbafcfe8a3cc4c8b2e762b
Nikolay Zhukov <nzhukov.rnd@gmail.com>
2022-05-12 18:59:25 +0300

    1

---
+++ WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Nikolay Zhukov on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
