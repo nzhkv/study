7782e6aebf4524e8bb0636cd40c37f8c2f3835d8
Nikolay Zhukov <nzhukov.rnd@gmail.com>
2022-05-12 19:03:29 +0300

    2

--- WeeklyFinder/WeeklyFinder/ViewController.swift
+++ WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
 }
 
