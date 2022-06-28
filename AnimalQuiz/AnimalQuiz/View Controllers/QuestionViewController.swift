0076972a346585827dcf7b945704529d01fc82fe
Nikolay Zhukov <nzhukov.rnd@gmail.com>
2022-06-28 23:01:00 +0300

    newView

---
+++ AnimalQuiz/AnimalQuiz/View Controllers/QuestionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionViewController.swift
+//  AnimalQuiz
+//
+//  Created by Nikolay Zhukov on 28.06.2022.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
