f5aeb285f9addee642240500845d86af4212ba69
Nikolay Zhukov <nzhukov.rnd@gmail.com>
2022-07-01 22:30:44 +0300

    back

--- LoginApp/LoginApp/SecondViewController.swift
+++ LoginApp/LoginApp/SecondViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var welcomeText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background.png")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
         
+        welcomeText.text = "Welcome"
+        welcomeText.textColor = .white
     }
     
 
- 
+    @IBAction func exitButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
 
 }
