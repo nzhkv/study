7462dedb0abd31a49ee99b4b53dd82e6133c7cfe
Nikolay Zhukov <nzhukov.rnd@gmail.com>
2022-07-01 17:35:27 +0300

    start

---
+++ LoginApp/LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Nikolay Zhukov on 01.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
